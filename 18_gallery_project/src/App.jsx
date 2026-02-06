cb181b8ec7994845bbd7186c891c20be466fbfbd
harshu-x <harshalpet07@gmail.com>
2026-02-06 21:35:20 +0530

    Initialize the project

---
+++ 18_gallery_project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black h-screen p-4 text-white'>
+      <button className='bg-green-600 active:scale-95 mb-3 px-5 py-2 text-white rounded'>Get Data</button>
+    </div>
+  )
+}
+
+export default App
