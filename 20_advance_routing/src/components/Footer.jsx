2a87a9a9c7dcf630aa13c7c7db9fb1a30e2eb89b
harshu-x <harshalpet07@gmail.com>
2026-02-10 11:50:10 +0530

    Routing

---
+++ 20_advance_routing/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=' absolute bottom-0 w-full bg-cyan-900 flex  items-center justify-center'>
+      Footer
+    </div>
+  )
+}
+
+export default Footer
